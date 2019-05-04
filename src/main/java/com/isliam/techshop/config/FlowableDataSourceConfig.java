7483c3e0980c70102cb30fc51e3e21d7a6f3ed2e
EvKator <islyambagirov@gmail.com>
2019-05-04 17:11:32 +0300

    domain managed by techShopDataSource

---
+++ src/main/java/com/isliam/techshop/config/FlowableDataSourceConfig.java
@@ -0,0 +1,41 @@
+package com.isliam.techshop.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+public class FlowableDataSourceConfig {
+
+
+
+    @Primary
+    @Bean(name = "flowableDataSource")
+    @ConfigurationProperties(prefix="spring.seconddatasource")
+    public DataSource secondaryDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.type(HikariDataSource.class);
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/flowable?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC&rewriteBatchedStatements=true&relaxAutoCommit=true");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("STRONGpwd1$");
+        return dataSourceBuilder.build();
+    }
+}
--- src/main/java/com/isliam/techshop/config/TechShopDatasourceConfig.java
+++ src/main/java/com/isliam/techshop/config/TechShopDatasourceConfig.java
@@ -1,29 +1,30 @@
 package com.isliam.techshop.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Configuration
-public class DataSourceConfiguration {
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    entityManagerFactoryRef = "entityManagerFactory",
+    transactionManagerRef = "transactionManager",
+    basePackages = { "com.isliam.techshop.repository" })
+public class TechShopDatasourceConfig {
 
     @Bean(name = "techShopDataSource")
     @ConfigurationProperties(prefix="spring.datasource")
@@ -36,15 +37,21 @@ public class DataSourceConfiguration {
         return dataSourceBuilder.build();
     }
 
-    @Primary
-    @Bean(name = "flowableDataSource")
-    @ConfigurationProperties(prefix="spring.seconddatasource")
-    public DataSource secondaryDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.type(HikariDataSource.class);
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/flowable?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC&rewriteBatchedStatements=true&relaxAutoCommit=true");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("STRONGpwd1$");
-        return dataSourceBuilder.build();
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean techShopEntityManagerFactory(
+        EntityManagerFactoryBuilder builder,
+        @Qualifier("techShopDataSource") DataSource techShopDataSource) {
+        return builder
+            .dataSource(techShopDataSource)
+            .packages("com.isliam.techshop.domain")
+            .persistenceUnit("bar")
+            .build();
     }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager techShopTransactionManager(
+        @Qualifier("entityManagerFactory") EntityManagerFactory barEntityManagerFactory) {
+        return new JpaTransactionManager(barEntityManagerFactory);
+    }
+
 }
