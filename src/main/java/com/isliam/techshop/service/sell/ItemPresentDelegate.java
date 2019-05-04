31b73bd71c2af6ff42a3b27e869471ab51c8b3ad
EvKator <islyambagirov@gmail.com>
2019-05-04 19:17:04 +0300

    simple provess

---
+++ src/main/java/com/isliam/techshop/service/sell/ItemPresentDelegate.java
@@ -0,0 +1,14 @@
+package com.isliam.techshop.service.sell;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+public class ItemPresentDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
