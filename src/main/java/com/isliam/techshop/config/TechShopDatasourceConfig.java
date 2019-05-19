358b5012b539577f638dfab281a3fc24e7f6b382
EvKator <islyambagirov@gmail.com>
2019-05-19 13:26:09 +0300

    autofill

--- src/main/java/com/isliam/techshop/config/TechShopDatasourceConfig.java
+++ src/main/java/com/isliam/techshop/config/TechShopDatasourceConfig.java
@@ -26,27 +26,27 @@ import javax.sql.DataSource;
     basePackages = { "com.isliam.techshop.repository" })
 public class TechShopDatasourceConfig {
 
-//    @Bean(name = "techShopDataSource")
-//    @ConfigurationProperties(prefix="spring.datasource")
-//    public DataSource primaryDataSource() {
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//        dataSourceBuilder.url("jdbc:sqlserver://kyrsa4.database.windows.net:1433;database=TechShop;user=root1@kyrsa4;password=STRONGpwd1$;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
-//        dataSourceBuilder.username("root1");
-//        dataSourceBuilder.password("STRONGpwd1$");
-//        return dataSourceBuilder.build();
-//    }
-//
     @Bean(name = "techShopDataSource")
     @ConfigurationProperties(prefix="spring.datasource")
     public DataSource primaryDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.type(HikariDataSource.class);
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/TechShop?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC&rewriteBatchedStatements=true&relaxAutoCommit=true");
-        dataSourceBuilder.username("root");
+        dataSourceBuilder.driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        dataSourceBuilder.url("jdbc:sqlserver://kyrsa4.database.windows.net:1433;database=TechShop;user=root1@kyrsa4;password=STRONGpwd1$;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+        dataSourceBuilder.username("root1");
         dataSourceBuilder.password("STRONGpwd1$");
         return dataSourceBuilder.build();
     }
+//
+//    @Bean(name = "techShopDataSource")
+//    @ConfigurationProperties(prefix="spring.datasource")
+//    public DataSource primaryDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.type(HikariDataSource.class);
+//        dataSourceBuilder.url("jdbc:mysql://localhost:3306/TechShop?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC&rewriteBatchedStatements=true&relaxAutoCommit=true");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("STRONGpwd1$");
+//        return dataSourceBuilder.build();
+//    }
 
 
     @Bean(name = "entityManagerFactory")
